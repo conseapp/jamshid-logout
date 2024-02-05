e1642c9cfaae79da1f17b9d35bc611aa214535d7
Armin Fekri <armiin.fekri1@gmail.com>
2024-02-06 02:43:39 +0330

    fixed token exists

--- utils/redis.py
+++ utils/redis.py
@@ -6,11 +6,14 @@ import logging
 async def logout(user_id: str, token: str, redis_credentials: RedisConnectioKeys):
     with RedisConnection(**redis_credentials, retries=3) as rd:
         key = f"jwt-{user_id}"
-        if rd.exists(key) and rd.get(key) == token:
-            # rd.delete(key)
-            logging.info(f'The key "{key}" has been successfully removed from Redis.')
-            logging.info(f"user {user_id} logged out")
-            return True
+        if rd.exists(key):
+            if rd.get(key) == token:
+                # rd.delete(key)
+                logging.info(f'The key "{key}" has been successfully removed from Redis.')
+                logging.info(f"user {user_id} logged out")
+                return True
+            else:
+                logging.error(f"logout failed, invalid token for user {user_id}")
         else:
             logging.error(f"logout failed, user {user_id} already logged out")
             return False
